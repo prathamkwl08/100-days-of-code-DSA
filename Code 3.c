99afc24e0a80891da547c98b302237c83afadaa2
Pratham Karnwal <pratham.25657@stu.upes.ac.in>
2026-02-15 05:39:35 +0530

    Add files via upload

---
+++ Code 3.c
@@ -0,0 +1,34 @@
+//Implement linear search to find key k in an array. Count and display the number of comparisons performed.
+
+#include <stdio.h>
+
+int main()
+{
+    int n, k;
+    int comparisons=0;
+    int found=0;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+    for(int i=0;i<n;i++)
+    {
+        comparisons++;
+        if(arr[i]==k)
+        {
+            printf("Found at index %d\n", i);
+            found=1;
+            break;
+        }
+    }
+    if(found==0)
+    {
+        printf("Not Found\n");
+    }
+    printf("Comparisons = %d", comparisons);
+    return 0;
+}
